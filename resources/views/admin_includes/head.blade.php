1bed84b0f6d94a240069a3ff45a489bca12a470b
swiftly01 <kareemkazeem100@gmail.com>
2024-12-24 19:23:19 +0100

    cache tag added

--- resources/views/admin_includes/head.blade.php
+++ resources/views/admin_includes/head.blade.php
@@ -1,6 +1,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate">
+  <meta http-equiv="Pragma" content="no-cache">
+  <meta http-equiv="Expires" content="0">
   <title>{{$title}}</title>
   <link rel="stylesheet" href="{{asset('kaz/css/bootstrap.css')}}">
   <link rel="stylesheet" href="{{asset('kaz/css/admin-nav.css')}}">

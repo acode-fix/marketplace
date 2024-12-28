b0de7ee54b5eda2c134b4471851b41f5aa44aa1e
swiftly01 <kareemkazeem100@gmail.com>
2024-12-28 03:19:58 +0100

    console removed

--- public/backend-js/rating.js
+++ public/backend-js/rating.js
@@ -271,9 +271,9 @@ const text = document.querySelector('.js-input');
 
   const formData = new FormData(reviewForm);
 
-   console.log([...formData]);
+//   console.log([...formData]);
 
-   return;
+
 
   const url = new URL(window.location.href);
   const userId = url.searchParams.get('user');

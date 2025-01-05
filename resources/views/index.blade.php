0edef9ac85838436deaa885beee8afabdfdb99ba
swiftly01 <kareemkazeem100@gmail.com>
2025-01-05 09:01:24 +0100

    title changed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Loopmart-marketPlace</title>
+    <title>Loopmart-marketplace</title>
     <link rel="icon" href="{{ asset('innocent/assets/image/favicon.svg') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('innocent/assets/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('innocent/assets/css/style.css') }}?time={{ time() }}">

bc6668799ee30746d050b8f49bf3077a83d634e2
Swiftly01 <kareemkazeem100@gmail.com>
2025-10-30 08:51:19 -0700

    refactor(payment): change API response to JSON instead of route redirect

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,193 +2,186 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Verification;
-use App\Models\Payment;
 use App\Models\BadgeUnit;
+use App\Models\Payment;
 use App\Models\User;
+use App\Models\Verification;
 use Barryvdh\Debugbar\Facades\Debugbar;
-use Illuminate\Support\Facades\DB;
+use Carbon\CarbonImmutable;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redirect;
-use Carbon\CarbonImmutable;
 
 class PaymentController extends Controller
-{   
-    public function badgeFeePaystackInit(Request $request) {
-        
+{
+    public function badgeFeePaystackInit(Request $request)
+    {
+
         $user = $request->user();
         $userId = $user->id;
         $userEmail = $user->email;
 
-       if($user) {
+        if ($user) {
 
-           $badgeType = $user->badge_type;
+            $badgeType = $user->badge_type;
 
-        if ($badgeType) {
+            if ($badgeType) {
 
-           $badgeDetails =  BadgeUnit::where('name', $badgeType)->first();
+                $badgeDetails = BadgeUnit::where('name', $badgeType)->first();
 
-           $amount = $badgeDetails->amount;
-           $purpose = $badgeDetails->purpose;
-           $desc = $badgeDetails->description;
+                $amount = $badgeDetails->amount;
+                $purpose = $badgeDetails->purpose;
+                $desc = $badgeDetails->description;
 
-           $txn = Payment::generateTxn();
-           $inv = Payment::generateInvoice();
+                $txn = Payment::generateTxn();
+                $inv = Payment::generateInvoice();
 
-           $payment = Payment::Create([
-                'user_id' => $userId,
-                'amount' => $amount,
-                'purpose' => $purpose,
-                'invoice_number' => $inv,
-                'transaction_reference' => $txn,
-                'description' => $desc,
-            ]);
+                $payment = Payment::Create([
+                    'user_id' => $userId,
+                    'amount' => $amount,
+                    'purpose' => $purpose,
+                    'invoice_number' => $inv,
+                    'transaction_reference' => $txn,
+                    'description' => $desc,
+                ]);
 
-           $amount_to_payStack = Payment::convinientfees($amount);
+                $amount_to_payStack = Payment::convinientfees($amount);
 
-           Debugbar::info($amount, $txn,$inv, $purpose, $desc,$amount, $amount_to_payStack);
+                Debugbar::info($amount, $txn, $inv, $purpose, $desc, $amount, $amount_to_payStack);
 
-           $response = $this->initPaystackTransaction($userEmail, $amount_to_payStack, $txn);
+                $response = $this->initPaystackTransaction($userEmail, $amount_to_payStack, $txn);
 
-           DebugBar::info($response);
+                DebugBar::info($response);
 
-           if(isset($response['data']['authorization_url'])) {
-            $paystack_url = $response['data']['authorization_url'];
+                if (isset($response['data']['authorization_url'])) {
+                    $paystack_url = $response['data']['authorization_url'];
 
-              return response()->json([
-                'status' => true,
-                'message' => 'payment url',
-                'paystack_url' => $paystack_url,
-              ]);
+                    return response()->json([
+                        'status' => true,
+                        'message' => 'payment url',
+                        'paystack_url' => $paystack_url,
+                    ]);
 
-           }else {
+                } else {
 
-             return response()->json([
-                'status' => false,
-                'message' => 'Failed to initialize payment with paystack',
-                
+                    return response()->json([
+                        'status' => false,
+                        'message' => 'Failed to initialize payment with paystack',
 
-             ], 500);
+                    ], 500);
 
+                }
 
-             }
+            } else {
 
-           
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Incompelete Verification Form',
 
-           } else {
+                ], 404);
 
-            return response()->json([
-                'status' => false,
-                'message' => 'Incompelete Verification Form',
+            }
 
-            ],404);
-
-           }
-        
-        }else {
+        } else {
 
             return response()->json([
                 'status' => false,
                 'message' => 'Incompelete Verification Form ',
 
-            ],404);
-
+            ], 404);
 
         }
-        
+
     }
 
-    public function initPaystackTransaction($userEmail,$amount_to_payStack, $txn,) {
+    public function initPaystackTransaction($userEmail, $amount_to_payStack, $txn)
+    {
 
-
-        $url = "https://api.paystack.co/transaction/initialize";
+        $url = 'https://api.paystack.co/transaction/initialize';
 
         $fields = [
             'email' => $userEmail,
-            'amount' =>  $amount_to_payStack * 100,
+            'amount' => $amount_to_payStack * 100,
             'reference' => $txn,
-            'callback_url' => env('APP_URL') . '/payment/callback',
-            
-            
+            'callback_url' => env('APP_URL').'/payment/callback',
+
         ];
 
         $fields_string = http_build_query($fields);
 
-        //open connection
+        // open connection
         $ch = curl_init();
-        
-        //set the url, number of POST vars, POST data
-        curl_setopt($ch,CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_POST, true);
-        curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Authorization: Bearer " . config('services.secret_key.key'),
-            "Cache-Control: no-cache",
-        ));
-        
-        //So that curl_exec returns the contents of the cURL; rather than echoing it
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
-        
-        //execute post
+
+        // set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: Bearer '.config('services.secret_key.key'),
+            'Cache-Control: no-cache',
+        ]);
+
+        // So that curl_exec returns the contents of the cURL; rather than echoing it
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // execute post
         $result = curl_exec($ch);
-        
+
         Debugbar::info($result);
 
         $response = json_decode($result, true);
 
-
         return $response;
-
 
     }
 
-    public function checkTransactionRef(Request $request) {
+    public function checkTransactionRef(Request $request)
+    {
 
         $transactionRef = $request->query('trxref');
 
-        Log::info($transactionRef); 
+        Log::info($transactionRef);
 
         $checkTrxRef = Payment::where('transaction_reference', $transactionRef)->first();
 
-        if($checkTrxRef) {
-                
+        if ($checkTrxRef) {
+
             return $this->verifyTransaction($transactionRef);
 
-        }else {
+        } else {
 
-         return   redirect()->route('success.page')->with('error', 'Invalid Transaction Reference!!');
+            //  return   redirect()->route('success.page')->with('error', 'Invalid Transaction Reference!!');
 
-           /* return response()->json([
+            return response()->json([
                 'status' => false,
-                'message' => 'Invalid Transaction Reference'
+                'message' => 'Invalid Transaction Reference',
             ], 404);
 
-            */
         }
 
     }
 
-    public function verifyTransaction($transactionRef) {
+    public function verifyTransaction($transactionRef)
+    {
 
-        //log::info($transactionRef);
+        // log::info($transactionRef);
 
         $curl = curl_init();
-        
-        curl_setopt_array($curl, array(
+
+        curl_setopt_array($curl, [
             CURLOPT_URL => "https://api.paystack.co/transaction/verify/$transactionRef",
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
+            CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-            "Authorization: Bearer " . config('services.secret_key.key'),
-            "Cache-Control: no-cache",
-            ),
-        ));
-        
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => [
+                'Authorization: Bearer '.config('services.secret_key.key'),
+                'Cache-Control: no-cache',
+            ],
+        ]);
+
         $responses = curl_exec($curl);
         $err = curl_error($curl);
 
@@ -196,141 +189,133 @@ class PaymentController extends Controller
 
         $response = json_decode($responses, true);
 
-       // Debugbar::info($responses);
-        
-        if ($err) {
-       /* echo "cURL Error #:" . $err;
-           return response()->json([
-            'status' => false,
-            'message' => 'Paystack Verification Failed',
-             'error' => $err,
+        // Debugbar::info($responses);
 
-           ], 400);
-       */
-          return redirect()->route('success.page')->with('error',  'Paystack Verification Failed:'. $err);
+        if ($err) {
+            // echo "cURL Error #:" . $err;
+                return response()->json([
+                 'status' => false,
+                 'message' => 'Paystack Verification Failed',
+                  'error' => $err,
+
+                ], 400);
+            
+          //  return redirect()->route('success.page')->with('error', 'Paystack Verification Failed:'.$err);
 
         } else {
-           //echo $responses;
+            // echo $responses;
 
-           if(array_key_exists('status', $response)) {
-               
-            //Log::info('exists');
+            if (array_key_exists('status', $response)) {
 
-            $status = $response['data']['status'];
-            $gatewayRes = $response['data']['gateway_response'];
-            $paymentDate = $response['data']['paid_at'];
-            $currency = $response['data']['currency'];
-            $transactionRef = $response['data']['reference'];
-            $amount = $response['data']['amount'];
+                // Log::info('exists');
 
-            Debugbar::info($response, $status, $gatewayRes, $currency,$paymentDate, $amount);
+                $status = $response['data']['status'];
+                $gatewayRes = $response['data']['gateway_response'];
+                $paymentDate = $response['data']['paid_at'];
+                $currency = $response['data']['currency'];
+                $transactionRef = $response['data']['reference'];
+                $amount = $response['data']['amount'];
 
-            if($status === 'success') {
+                Debugbar::info($response, $status, $gatewayRes, $currency, $paymentDate, $amount);
 
-                $storeTransaction = [
-                    'status' => 1,
-                    'gateway_response' => $gatewayRes,
-                    'payment_date' => $paymentDate,
-                    'currency' => $currency,
-                ];
+                if ($status === 'success') {
 
-                $payment = Payment::where('transaction_reference', $transactionRef)->first();
+                    $storeTransaction = [
+                        'status' => 1,
+                        'gateway_response' => $gatewayRes,
+                        'payment_date' => $paymentDate,
+                        'currency' => $currency,
+                    ];
 
-            
-                if($payment) {
+                    $payment = Payment::where('transaction_reference', $transactionRef)->first();
 
-                    $userId = $payment->user_id;
+                    if ($payment) {
 
-                    //change status to pending;
+                        $userId = $payment->user_id;
 
-                    $user = User::where('id', $userId)->where('verify_status', 0)->first();
+                        // change status to pending;
 
-                    if($user) {
-                        
-                        $user->verify_status = -2;
-                        $user->save();
+                        $user = User::where('id', $userId)->where('verify_status', 0)->first();
+
+                        if ($user) {
+
+                            $user->verify_status = -2;
+                            $user->save();
+
+                        }
+
+                        $user = User::where('id', $userId)->where('verify_status', 1)->first();
+
+                        if ($user) {
+
+                            $purchaseDate = CarbonImmutable::now();
+                            $expiryDate = Payment::getExpiryDate($user->badge_type);
+
+                            $badgeDetails = [
+                                'purchase_date' => $purchaseDate,
+                                'expiry_date' => $expiryDate,
+                                'badge_status' => 1,
+                            ];
+
+                            $user->update($badgeDetails);
+
+                        }
+
+                        $payment->update($storeTransaction);
+
+                        /* return redirect()->route('success.page')->with(
+                             'message','Transaction Successful'); */
+                        return response()->json([
+                            'status' => true,
+                            'message' => 'Transaction completed successfully',
+                            'data' => $badgeDetails,
+
+                        ]);
+
+                    } else {
+
+                        //  return redirect()->route('success.page')->with('error', 'Transaction Details Could Not Be Saved!!');
+
+                        return response()->json([
+                            'status' => false,
+                            'message' => 'Transaction could not be saved',
+                        ], 404);
 
                     }
-
-                    $user = User::where('id',$userId)->where('verify_status', 1)->first();
-
-                    if($user) {
-
-                        $purchaseDate = CarbonImmutable::now();
-                        $expiryDate = Payment::getExpiryDate($user->badge_type);
-
-                        $badgeDetails = [
-                            'purchase_date' => $purchaseDate,
-                            'expiry_date' => $expiryDate,
-                            'badge_status' => 1,
-                        ];
-
-                        $user->update($badgeDetails);
-
-                    }
-
-                   
-
-                    $payment->update($storeTransaction);
-
-                    return redirect()->route('success.page')->with(
-                        'message','Transaction Successful');
-
 
                 } else {
 
-                    return redirect()->route('success.page')->with('error', 'Transaction Details Could Not Be Saved!!');
+                    // return redirect()->route('success.page')->with('error', 'Paystack Status Verification Failed!!');
 
-                   /* return response()->json([
+                    return response()->json([
                         'status' => false,
-                        'message' => 'Transaction could not be saved',
-                    ],404);
-                    */
+                        'message' => 'Paystack Status Failed',
+                    ], 400);
 
                 }
 
+            } else {
 
+                // return redirect()->route('success.page')->with('error', 'Paystack Status Key Is Empty!!');
 
-            }else {
-
-                return redirect()->route('success.page')->with('error', 'Paystack Status Verification Failed!!');
-               /*
                 return response()->json([
                     'status' => false,
-                    'message' => 'Paystack Status Failed',
-                   ], 400);
-              */
+                    'message' => 'Paystack Status Key Empty',
+                    'response' => $response,
+
+                ], 400);
+
             }
-
-            
-           }else {
-
-            return redirect()->route('success.page')->with('error', 'Paystack Status Key Is Empty!!');
-
-          /*  return response()->json([
-                'status' => false,
-                'message' => 'Paystack Status Key Empty',
-                'response' => $response,
-    
-               ], 400);
-            */
-
-           }
-
-
 
         }
 
-
     }
 
-    public function successPage() {
+    public function successPage()
+    {
 
         return view('other_frontend.payment_success');
     }
-
-
-
 
     /**
      * Show the form for creating a new resource.

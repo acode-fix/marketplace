6fa4d6165cee5c917f00014cf07d20f80d8c6c4d
Swiftly01 <kareemkazeem100@gmail.com>
2026-01-08 11:44:21 +0100

    feat(model): add user and product relationship to product engagement log

--- app/Models/ProductEngagementLog.php
+++ app/Models/ProductEngagementLog.php
@@ -10,4 +10,13 @@ class ProductEngagementLog extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
